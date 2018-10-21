8365f89ed917b7c65190b507bbe8185a79a08490
Steve Faulkner <southpolesteve@gmail.com>
2018-10-20 22:15:00 -0700

    feat: reduce browser bundle size from 9.15kb to 2.66

--- with-defaults.js
+++ with-defaults.js
@@ -1,11 +1,11 @@
 module.exports = withDefaults
 
-const defaultsDeep = require('lodash/defaultsDeep')
+const merge = require('deepmerge')
 
 const endpointWithDefaults = require('./lib/endpoint-with-defaults')
 
 function withDefaults (oldDefaults, newDefaults) {
-  const DEFAULTS = defaultsDeep({}, newDefaults, oldDefaults)
+  const DEFAULTS = merge.all([oldDefaults, newDefaults].filter(Boolean))
   const endpoint = endpointWithDefaults.bind(null, DEFAULTS)
   endpoint.DEFAULTS = DEFAULTS
   endpoint.defaults = withDefaults.bind(null, DEFAULTS)
