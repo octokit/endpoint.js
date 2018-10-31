d3e4f1d4e65c7d5bba32567d4b92aa1a836d64e7
Gregor <gregor@martynus.net>
2018-10-30 21:21:33 -0700

    fix: only merge simple objects (part II)

--- with-defaults.js
+++ with-defaults.js
@@ -1,11 +1,12 @@
 module.exports = withDefaults
 
 const merge = require('deepmerge')
+const isPlainObject = require('is-plain-object')
 
 const endpointWithDefaults = require('./lib/endpoint-with-defaults')
 
 function withDefaults (oldDefaults, newDefaults) {
-  const DEFAULTS = merge.all([oldDefaults, newDefaults].filter(Boolean))
+  const DEFAULTS = merge.all([oldDefaults, newDefaults].filter(Boolean), { isMergeableObject: isPlainObject })
   const endpoint = endpointWithDefaults.bind(null, DEFAULTS)
   endpoint.DEFAULTS = DEFAULTS
   endpoint.defaults = withDefaults.bind(null, DEFAULTS)
