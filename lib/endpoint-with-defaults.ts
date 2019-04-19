f3130d77ebffb63a0e1a4d30b74d8f1dde376ce7
wolfy1339 <webmaster@wolfy1339.com>
2019-04-19 19:43:47 -0400

    Add better types for endpointWithDefaults options param

--- lib/endpoint-with-defaults.ts
+++ lib/endpoint-with-defaults.ts
@@ -3,6 +3,6 @@ export = endpointWithDefaults
 import merge = require('./merge')
 import parse = require('./parse')
 
-function endpointWithDefaults (defaults: typeof import("./defaults"), route: string, options: any) {
-  return parse(merge(defaults, route, options))
+function endpointWithDefaults (defaults: typeof import("./defaults"), route: string, options: { method?: string; url?: string; [key: string]: any }) {
+  return parse(merge(defaults, route!, options))
 }
