dd43d9e38d1be9defd1d9bdd5477625194ae5f03
Gregor <gregor@martynus.net>
2019-05-08 10:19:53 -0700

    build(typescript): remove external type definitions for is-plain-object

--- src/external.d.ts
+++ src/external.d.ts
@@ -1,6 +1,3 @@
 declare module "universal-user-agent" {
   export default function getUserAgentNode(): string;
 }
-declare module "is-plain-object" {
-  export default function isPlainObject(o: any): boolean;
-}
