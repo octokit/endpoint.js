120081ddd958669fba59c374d2e3092b0f14c872
wolfy1339 <webmaster@wolfy1339.com>
2023-11-28 18:21:36 -0500

    chore: remove accidental code

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,3 @@ import { withDefaults } from "./with-defaults.js";
 import { DEFAULTS } from "./defaults.js";
 
 export const endpoint = withDefaults(null, DEFAULTS);
-
-fetch("https://api.github.com/users/octocat", {
-  headers: {
-    accept: "application/vnd.github.v3+json",
-  },
-  method: "GET",
-})
